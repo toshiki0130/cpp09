9f550abc1025e5e3b3c616e1069bfd808f0842d5
Toshiki Sakai <tosshi18@gmail.com>
2025-02-25 05:30:07 +0900

    WIP: validate

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <cerrno>
+#include <climits>
 #include "PmergeMe.hpp"
 
 void run_sort(std::vector<int> &orig) {
@@ -40,10 +42,9 @@ void run_sort(std::vector<int> &orig) {
     }
     std::cout << std::endl;
     std::cout << "Time to process a range of " << v.size() 
-        << "elements with std::vector: " << std::fixed << std::setprecision(6) << elapsed_v << "s\n" << std::endl;
-    std::cout << std::endl;
+        << " elements with std::vector : " << std::fixed << std::setprecision(6) << elapsed_v << "s" << std::endl;
     std::cout << "Time to process a range of " << d.size() 
-        << "elements with std::deque: " << std::fixed << std::setprecision(6) << elapsed_d << "s\n" << std::endl;
+        << " elements with std::deque : " << std::fixed << std::setprecision(6) << elapsed_d << "s" << std::endl;
 }
 
 int stringToDouble(const std::string &str) throw(std::invalid_argument) {
@@ -58,7 +59,55 @@ int stringToDouble(const std::string &str) throw(std::invalid_argument) {
     return result;
 }
 
+bool is_positive_int(const std::string& arg){
+    if (arg.empty()) {
+        return false;
+    } 
+    if (arg[0] == '-') {
+        return false;
+    }
+    char *end;
+    errno = 0;
+    long nbr = std::strtol(arg.c_str(), &end, 10);
+    if (*end != '\0') {
+        // 文字列が混ざってる
+        return false;
+    }
+    if (nbr == 0) {
+        return false;
+    }
+    if (errno == ERANGE || nbr > INT_MAX) {
+        // INTの範囲外
+        return false;
+    }
+    return true;
+}
+
+void validateArgs(int argc, char *argv[]) throw(std::invalid_argument) {
+    if (argc < 2) {
+        throw std::invalid_argument("No arguments were provided");
+    }
+    for (int i = 1; i < argc; i++) {
+        bool result = is_positive_int(argv[i]);
+        if (!result) {
+            throw std::invalid_argument("Not positive integer or Not invalid range");
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
+    try {
+        std::cout << INT_MAX << std::endl;
+        validateArgs(argc, argv);
+    }
+    catch (const std::invalid_argument& e) {
+        std::cerr << "Error: invalid arguments -> " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: Unexpected Error -> " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
     std::vector<int> orig;
     for (int i=1; i < argc; i++) {
         orig.push_back(stringToDouble(argv[i]));
